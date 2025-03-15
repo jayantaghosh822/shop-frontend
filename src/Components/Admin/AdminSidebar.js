a9503c62b0dcb81839d6f17e0860b5673d2d264a
Jayanta Ghosh <jayantaghosh822@gmail.com>
2025-03-15 12:52:26 +0530

    Initial commit1

--- src/Components/Admin/AdminSidebar.js
+++ src/Components/Admin/AdminSidebar.js
@@ -75,8 +75,32 @@ const AdminSidebar = ()=>{
                 <span className="nav-link-text ms-1">Tables</span>
               </a>
             </li>
+
             <li className="nav-item">
-               <Link to="/admin/categories/create" className="nav-link">
+               <Link to="/admin/category/" className="nav-link">
+             
+                <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                  <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                    <title>credit-card</title>
+                    <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                      <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fillRule="nonzero">
+                        <g transform="translate(1716.000000, 291.000000)">
+                          <g transform="translate(453.000000, 454.000000)">
+                            <path className="color-background opacity-6" d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z" />
+                            <path className="color-background" d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z" />
+                          </g>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </div>
+                <span className="nav-link-text ms-1">Categories</span>
+                </Link>
+                
+            </li>
+
+            <li className="nav-item">
+               <Link to="/admin/category/create" className="nav-link">
              
                 <div className="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                   <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
@@ -95,6 +119,7 @@ const AdminSidebar = ()=>{
                 </div>
                 <span className="nav-link-text ms-1">Create Category</span>
                 </Link>
+                
             </li>
             <li className="nav-item">
               <a className="nav-link  " href="../pages/virtual-reality.html">
--- src/Pages/Admin/AllCategory.js
+++ src/Pages/Admin/AllCategory.js
@@ -2,24 +2,51 @@ import React from 'react';
 import { useState , useEffect } from 'react';
 import axios from 'axios';
 import { Outlet, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 const AllCategories = ()=>{
    const [Categories , setCategories] = useState([]);
+
+    const fetchCategories = async ()=>{
+    const backendUrl = process.env.REACT_APP_BACKEND_URL;
+    const response = await axios.get(`${backendUrl}/api/categories/`);
+    setCategories(response.data.categories);
+    }
+    
    useEffect(()=>{
-     const fetchCategories = async ()=>{
-        const backendUrl = process.env.REACT_APP_BACKEND_URL;
-        const response = await axios.get(`${backendUrl}/api/categories/`);
-        setCategories(response.data.categories);
-     }
      fetchCategories();
    },[])
 
    const deleteCategory= async(id)=>{
     const backendUrl = process.env.REACT_APP_BACKEND_URL;
     const response = await axios.delete(`${backendUrl}/api/category/delete/${id}`);
+    console.log(response);
+    if(response.data.success){
+        toast.success(response.data.message, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        fetchCategories();
+    }else{
+        toast.error(response.data.message, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
    }
     return(
         <>
+        <ToastContainer />
         <table class="table">
             <thead>
                 <tr>
@@ -33,7 +60,7 @@ const AllCategories = ()=>{
                     <tr>
                     <td>{elem.name}</td>
                     <td>{elem.slug}</td>
-                    <td><Link to={`edit/${elem.slug}`}>Edit</Link> <a href="javascript:void(0);" onClick={() => deleteCategory(elem._id)}>Delete</a></td>
+                    <td><Link to={`edit/${elem.slug}`}>Edit</Link> <button href="" onClick={() => deleteCategory(elem._id)}>Delete</button></td>
                     </tr>
                 );
             }) }
